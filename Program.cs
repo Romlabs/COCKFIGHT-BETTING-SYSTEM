2bd182f4d04b1dd19bdaace2194e513a264fea9e
RIOABLAS <r.ablas.514023@umindanao.edu.ph>
2026-03-07 17:02:16 +0800

    Wrote CLI code for reference.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,35 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Jumpstart();
+    }
+
+    static void Jumpstart()
+    {
+        do
+        {
+            Console.WriteLine("1. Login Bettor\n" +
+            "2. Register Bettor\n" +
+            "3. Exit\n" +
+            "Enter Choice: ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+
+                break;
+                case "2":
+
+                break;
+                case "3":
+                    Environment.Exit(0);
+                break;
+                default:
+                    Console.WriteLine("Please enter a valid input!");
+                break;
+            }
+
+        } while(true);
     }
 }
+
