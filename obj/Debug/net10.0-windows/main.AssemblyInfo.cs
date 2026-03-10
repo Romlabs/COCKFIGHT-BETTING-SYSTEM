fad3178b18e968f5b6bb71731a32c4ca0836186d
Romlabs <rommelyasay8@gmail.com>
2026-03-10 09:04:14 +0800

    some changes are made to the main project, and I want to commit those changes with a message describing what was changed. The commit message should be concise and informative, summarizing the key changes made to the project.

--- obj/Debug/net10.0-windows/main.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b586b125a3961cf163030c8e7fef885cd89e0435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c587a347b883aae01c16f7c014e10e36577be562")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
