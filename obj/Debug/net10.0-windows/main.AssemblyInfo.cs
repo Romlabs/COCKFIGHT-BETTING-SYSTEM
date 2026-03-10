20ae461c18e6d48c39e6843919aa4c1b2d30a494
Romlabs <rommelyasay8@gmail.com>
2026-03-10 10:04:46 +0800

    modified main/obj/Debug/net10.0-windows/main.AssemblyInfo.cs

--- obj/Debug/net10.0-windows/main.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c587a347b883aae01c16f7c014e10e36577be562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad3178b18e968f5b6bb71731a32c4ca0836186d")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
